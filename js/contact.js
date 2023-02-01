a4efccd12fdb983ea07cd360447a432d9371d225
Zijie <s10247488@connect.np.edu.sg>
2023-02-01 21:54:19 +0800

    Updated interface and login/sign in

--- js/contact.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-	$("body").on('click', '.top', function() {
-		$("nav.menu").toggleClass("menu_show");
-	});
-});
--- js/login.js
+++ js/login.js
@@ -1,5 +1,11 @@
-$(document).ready(function() {
-	$("body").on('click', '.top', function() {
-		$("nav.menu").toggleClass("menu_show");
-	});
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
 });
